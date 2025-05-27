a3264607ddb215af0e1ce5e1b2a48ca38c735040
Josh Nussbaum <joshnuss@gmail.com>
2025-05-26 22:29:27 -0400

    Adds prettier

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,10 +1,12 @@
 import eslint from '@eslint/js'
 import { globalIgnores } from 'eslint/config'
 import tseslint from 'typescript-eslint'
+import prettierConfig from 'eslint-config-prettier'
 
 export default tseslint.config(
-  globalIgnores(["dist/*"]),
+  globalIgnores(['dist/*']),
   eslint.configs.recommended,
   tseslint.configs.strict,
   tseslint.configs.stylistic,
+  prettierConfig
 )
